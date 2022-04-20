b42e0fc5f093a7a3d5561d0f39c2be08f796e435
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-04-19 21:24:00 -0300

    Video 2 y 3 hechos

--- Assets/Scripts/EJ2MayoriaDeEdad.cs
+++ Assets/Scripts/EJ2MayoriaDeEdad.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EJ2MayoriaDeEdad : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int edadUsuario;
+    public Text miTexto;
+
     void Start()
     {
-        
+        if (edadUsuario >= 18)
+        {
+            miTexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+
+        else
+        {
+            miTexto.text = "No es mayor de edad";
+            Debug.Log("No es mayor de edad");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
